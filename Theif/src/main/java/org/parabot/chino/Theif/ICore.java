297d37d24f96f8d607b256f3e65604cf91ab214d
chinopuente <joseph.bozzi16@gmail.com>
2018-09-02 16:50:47 -0400

    Reformat

---
+++ Theif/src/main/java/org/parabot/chino/Theif/ICore.java
@@ -0,0 +1,12 @@
+package org.parabot.chino.Theif;
+
+
+import org.parabot.chino.Theif.utils.Methods;
+
+
+public interface ICore {
+
+    org.parabot.chino.Theif.data.Settings getSettings();
+
+    Methods getMethods();
+}
